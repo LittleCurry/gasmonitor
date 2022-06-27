a1f3905928ea7d8c328c000dded1d1a2968b68a5
Luoxi <42119836+rossixiluo@users.noreply.github.com>
2022-06-27 16:11:08 +0800

    decimal

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ for(let i = 1; i < table.rows.length; i++){
                     balance = web3.utils.fromWei(wei, 'ether');
                     //var balance = wei;
                     
-                    table.rows[i].cells[4].innerHTML = balance.toFixed(5);
+                    table.rows[i].cells[4].innerHTML = balance;
                     console.log(chainid);
                     console.log(balance);
                     console.log(threshold);
